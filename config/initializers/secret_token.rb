8481493627e78656c98d8adb7a11915868da3f45
Ankit Parasher <a.nkitparasher@gmail.com>
2013-12-03 05:16:22 -0500

    Zeroth commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Imart::Application.config.secret_token = '75bad094eef2634d93c0008569afcdb69059871f8a9d7a12e7e406cbbbc0f5a86fbb232a16c575419be9a0275ff915092a1efc51981edf9a62eb27ff4bc33c8d'
